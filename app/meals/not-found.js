57f23618601df73132af584c9c25106bd2850060
unknown <rohelahi.shakiba.pania@gmail.com>
2025-08-20 14:46:16 +0330

    initial commit

---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+ export default function NotFound(){
+    return (
+        <main>
+            <h1> Meal Not Found</h1>
+            <p>Unfortunately , we could not find the request page or resource.</p>
+        </main>
+    )
+ }
